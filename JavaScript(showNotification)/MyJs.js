47eba950c212627f2a4e971b3fc0cda54ba79b51
ZaharovIgor <zaharovigorzp@gmail.com>
2017-03-28 00:06:43 +0300

    HomeWork 27.03

--- JavaScript(showNotification)/MyJs.js
+++ JavaScript(showNotification)/MyJs.js
@@ -2,46 +2,41 @@ var success = document.querySelector('.success');
 var error = document.querySelector('.error');
 
 function showNatification(obj) {
-	obj.time = obj.time || 2000;
-	if(obj.type === 'success') {
-		if( $('div').hasClass('footer') ) $('div').removeClass('footer');
-		$('div').addClass('header');
-		$('div').text(obj.text);
-
-		setTimeout(funcSuccess, obj.time);
-		
-	} else { 
-		if( $('div').hasClass('header')) $('div').removeClass('header');
-		$('div').addClass('footer');
-		$('div').text(obj.text);
-
-		setTimeout(funcError, obj.time);
+	var hiddenDiv = document.createElement('div');
+	hiddenDiv.textContent = obj.text;
+	document.body.appendChild(hiddenDiv);
+	if(obj.bottom) {
+		hiddenDiv.style.bottom = 0;
+	} else {
+		hiddenDiv.style.top = 0;
 	}
+	
+	if (obj.type === 'success') {
+		hiddenDiv.classList.add("header");
+	} else {
+		hiddenDiv.classList.add("footer");
+	}	
+
+	function myFunc() {
+		hiddenDiv.parentNode.removeChild(hiddenDiv);
+	}
+	setTimeout(myFunc.bind(obj), obj.time);
 }
 
-function funcSuccess() {
-	$('div').removeClass('header');
-	$('div').text('');
-}
-
-function funcError() {
-	$('div').removeClass('footer');
-	$('div').text('');		
-}
 
 success.onclick = function() {
 	showNatification({
 		type: 'success',
 		text: 'Some message! Some message!',
-		// bottom: false,
-		// time: 1000
+		bottom: true,
+		time: 1000
 	});
 }
 error.onclick = function() {
 	showNatification({
 		type: 'error',
 		text: 'Text of our Notification',
-		// bottom: true,
-		// time: 3000
+		bottom: false,
+		time: 3000
 	});
 }
