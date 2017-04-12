0eba8631f3e819997a7a1cc841dea06c610642f4
Vlad Povarenko <povarenko@icloud.com>
2017-04-12 21:17:36 +0300

    Ajax

--- Ajax/script.js
+++ Ajax/script.js
@@ -6,13 +6,16 @@ btn.onclick = function(e) {
 	var form = document.forms.randomuser;
 	var sum = form.elements.sum.value;
 	var preloader = document.querySelector('.loader')
+	var table = document.querySelector('table')
 	
 	fetch( 'https://randomuser.me/api/?results=' + sum, {
       method: 'GET',
     }).then(function(response) {
     	preloader.classList.toggle('hidden');
+    	form.classList.toggle('hidden');
       return response.json();
     }).then(function(data) {
+    	table.classList.toggle('hidden');
     	renderUsers(data)
     	preloader.classList.toggle('hidden');
     });
@@ -29,6 +32,7 @@ function renderUsers(data) {
 			$('table').append(html);
 		}
 	);
+
 }
 
 
