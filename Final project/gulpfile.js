81a0bebe7b2c44791e7ce9ea4dae9e7a0285b211
rustem <007.1988@mail.ru>
2017-05-04 17:24:04 +0300

    New

---
+++ Final project/gulpfile.js
@@ -0,0 +1,9 @@
+﻿var gulp = require('gulp'),  
+sass = require('gulp-sass');
+
+gulp.task('sass', function(){
+	return gulp.src('app/sass/main.sass')
+	.pipe(sass())
+	.pipe(gulp.dest('app/css'))
+});
+
